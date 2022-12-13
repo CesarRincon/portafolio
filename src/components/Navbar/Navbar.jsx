570cba02218cf52e1fc36d1298244f05829676e4
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2022-12-13 12:03:56 -0500

    Menu responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,20 @@ import s from './Navbar.module.css'
 import { NavLink, Link } from 'react-router-dom'
 
 export default function Navbar() {
+
+
+  const activeNav = () => {
+    let ul = document.getElementById('NavUl');
+    if (ul.style.display === "flex") return ul.style.display = "none";
+    ul.style.display = "flex";
+  }
+
+
   return (
     <nav className={s.navbar}>
       <h1><Link to={'/'}>Cesar Rincon</Link></h1>
-      <ul>
+      <span className={s.buttonMenu} onClick={() => activeNav()}>Menu</span>
+      <ul id='NavUl'>
         <li><NavLink className={({isActive}) => isActive ? s.active : "" } to={'/technologies'}>Technologies</NavLink></li>
         <li><NavLink className={({isActive}) => isActive ? s.active : "" } to={'/works'}>Works</NavLink></li>
         <li><NavLink className={({isActive}) => isActive ? s.active : "" } to={'/About'}>About Me</NavLink></li>
