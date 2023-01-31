d57a8691546b853ffa776129085dc7aad7749de1
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2023-01-31 18:25:53 -0500

    feat: Cambio de titulo de works a works prueba

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion"
 export default function Works() {
   return (
     <motion.div initial={{width: 0}} animate={{width: "100%"}} exit={{x: window.innerWidth}} className={s.containerW}>
-      <h1 style={{color: "white"}}>Works</h1>
+      <h1 style={{color: "white"}}>Works Prueba</h1>
     </motion.div>
   )
 }
