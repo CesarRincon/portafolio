e816c54232e0c10e8366b84bba07aa3287726243
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2024-10-01 09:58:00 -0500

    new portfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Landing from './pages/Landing/Landing';
-import NotFound from './components/NotFound/NotFound';
-import Works from './components/Works/Works';
-import About from './components/About/About';
-import Home from './pages/Home/Home';
-import AnimateRoutes from './components/AnimatedRoutes/AnimateRoutes';
-import Navbar from './components/Navbar/Navbar';
-
+import Landing from "./pages/Landing/Landing";
+import NotFound from "./components/NotFound/NotFound";
+import Works from "./components/Works/Works";
+import About from "./components/About/About";
+import Home from "./pages/Home/Home";
+import AnimateRoutes from "./components/AnimatedRoutes/AnimateRoutes";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <AnimateRoutes/>
+    <div className="app">
+      {/* <Navbar /> */}
+      <AnimateRoutes />
     </div>
-
   );
 }
 
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,18 +1,43 @@
-import Navbar from '../../components/Navbar/Navbar'
-import s from './Landing.module.css'
-import { motion } from "framer-motion"
-import Particle from '../../components/Particles/Particle'
-
+import Navbar from "../../components/Navbar/Navbar";
+import style from "./Landing.module.css";
+import { motion } from "framer-motion";
+import Particle from "../../components/Particles/Particle";
 
 export default function Landing() {
-
-    
-
-
-    return (
-        <motion.div intial={{ width: 0 }} animate={{ width: "100%" }} exit={{ x: window.innerWidth }}>
-            <Particle/>
-            <div className={s.landing}>
+  return (
+    <motion.div
+      intial={{ width: 0 }}
+      animate={{ width: "100%" }}
+      exit={{ x: window.innerWidth }}
+      style={{ height: "100%" }}
+    >
+      <Particle />
+      <div className={style.landing}>
+        <div className={style.container}>
+          <div className={style.leftMenuContainer}>
+            <div className={style.items}>1</div>
+            <div className={style.items}>2</div>
+            <div className={style.items}>3</div>
+          </div>
+          <div className={style.contentContainer}>
+            <div className={style.containerDescription}>
+              <div className={style.aboutMeContainer}></div>
+              <div className={style.aboutMeContainer}></div>
+              <div className={style.contentDescription}></div>
+            </div>
+            <div className={style.imageContainer}>
+              <div className={style.imageContent}>
+                <img
+                  src="https://hackmd-prod-images.s3-ap-northeast-1.amazonaws.com/uploads/upload_dcf5c9c18fe578f354ce9a4fe609e4cc.jpg?AWSAccessKeyId=AKIA3XSAAW6AWSKNINWO&Expires=1727470558&Signature=3%2Bv90AW%2Bb94xYlklwWpg7gDGJmo%3D"
+                  alt="CesarRincon"
+                  className={style.image}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className={s.landing}>
                 <div className={`${s.sideLeft} ${s.sides}`}>
                     <h1>Full Stack Developer</h1>
                     <p>
@@ -29,7 +54,7 @@ export default function Landing() {
 
                     </div>
                 </div>
-            </div>
-        </motion.div>
-    )
+            </div> */}
+    </motion.div>
+  );
 }
